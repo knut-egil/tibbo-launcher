e3a872b0cc47721be57175dd3ad53232492536b9
ninjasploit <ninjasploit@users.noreply.github.com>
2024-01-28 18:05:57 +0100

    Init

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,7 @@ namespace TibboLauncher
 {
     public class LauncherViewModel : INotifyPropertyChanged
     {
+        #region Progress
         private float _downloadProgress;
         public float DownloadProgress
         {
@@ -33,7 +36,7 @@ namespace TibboLauncher
                 _downloadProgress = value;
 
                 // Notify property changes
-                NotifyPropertyChanged("DownloadProgress");
+                NotifyPropertyChanged(); // Notify of "DownloadProgress" change
                 NotifyPropertyChanged("StatusText"); // This tells the UI to also update the StatusText when DownloadProgress changes
             }
         }
@@ -45,7 +48,60 @@ namespace TibboLauncher
                 return $"Downloading... ({DownloadProgress}%)";
             }
         }
+        #endregion
 
+        #region Version
+        private string _version = "0.0.0";
+        /// <summary>
+        /// Raw version string as reported from backend server
+        /// </summary>
+        public string Version
+        {
+            get
+            {
+                return _version;
+            }
+            set
+            {
+                _version = value;
+                NotifyPropertyChanged();
+                NotifyPropertyChanged("VersionText");
+            }
+        }
+        
+        /// <summary>
+        /// Formatted version text
+        /// </summary>
+        public string VersionText
+        {
+            get
+            {
+                return $"v{Version}";
+            }
+        }
+        #endregion
+
+        #region Changelog
+        private string _changelog = "Changelog...";
+        /// <summary>
+        /// Changelog as reported from backend server
+        /// </summary>
+        public string Changelog
+        {
+            get
+            {
+                return _changelog;
+            }
+            set
+            {
+                _changelog = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implement
         public event PropertyChangedEventHandler? PropertyChanged;
         protected virtual void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
@@ -53,6 +109,7 @@ namespace TibboLauncher
             if (handler != null)
                 handler(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
     }
 
     /// <summary>
@@ -65,6 +122,45 @@ namespace TibboLauncher
         {
             DataContext = _viewModel;
             InitializeComponent();
+        }
+    }
+
+    /// <summary>
+    /// Easy-to-use Tibbo API wrapper
+    /// </summary>
+    public static class TibboAPI
+    {
+        private static class APIEndpoints
+        {
+            public static Uri Base => new Uri("https://webhook.site/4c8f3912-073a-449b-a0fa-caa8130a0efc/api");
+            public static Uri Version => new Uri(Base, "/version");
+        }
+
+        /// <summary>
+        /// Get the latest game-client version
+        /// </summary>
+        /// <returns>Latest game version</returns>
+        public static async Task<string?> GetCurrentVersion()
+        {
+            try
+            {
+                // Make request to Tibbo current-version endpoint
+                using (var client = new HttpClient())
+                {
+                    // Get result
+                    string result = await client.GetStringAsync(APIEndpoints.Version);
+
+                    // Return
+                    return result;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Encountered an error while requesting the latest Tibbo game version. Error: {ex}");
+            }
+
+            // Return null by default
+            return null;
         }
     }
 }
